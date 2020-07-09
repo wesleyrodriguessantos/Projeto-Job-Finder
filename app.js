a9ad5eb51c470dc0344baae781966a0dab903197
Wesley Rodrigues <wesleyrodriguessantos99@gmail.com>
2020-07-09 16:19:20 -0300

    Iniciando o app com o Express

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, function () {
+    console.log(`O Express está rodando na porta ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+    res.send("Está funcionando 3!");
+});
+
+// Nodemon atualiza a aplicação sem ter que reiniciar o Servidor
