a11ffeb2338f3b551959127fddcf22f9a475f25d
Wesley Rodrigues <wesleyrodriguessantos99@gmail.com>
2020-07-11 15:24:09 -0300

    lógica da busca

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const path = require('path');
 const db = require('./db/connection');
 const bodyParser = require('body-parser');
 const Job = require('./models/Job');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 const PORT = 3000;
 
@@ -32,16 +34,40 @@ db.authenticate().then(() => {
 
 // Routes
 app.get('/', (req, res) => {
-    Job.findAll({order: [
-        ['createdAt', 'DESC']
-    ]}).then(jobs => {
-       
-       
-        res.render('index', { jobs
-            
-        });
 
-    });
+    let search = req.query.job;
+    let query = '%'+search+'%';
+
+    if (!search) {
+        Job.findAll({order: [
+            ['createdAt', 'DESC']
+        ]}).then(jobs => {
+        
+        
+            res.render('index', { jobs
+                
+            });
+
+        })
+        .catch(err => console.log(err));
+        
+    } else {
+
+        Job.findAll({
+            where: {title: {[Op.like]: query}},
+            order: [
+            ['createdAt', 'DESC']
+        ]}).then(jobs => {
+        
+        
+            res.render('index', { 
+                jobs, search
+            });
+
+        })
+        .catch(err => console.log(err));
+
+    }
     
 });
 
